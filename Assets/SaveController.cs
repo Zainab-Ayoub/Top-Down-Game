da0ce24e1de26c2f260a2c3f6928c09d8429b5d4
Zainab-Ayoub <zainabzafar365@gmail.com>
2025-07-25 03:02:07 +0500

    load game

--- Assets/SaveController.cs
+++ Assets/SaveController.cs
@@ -11,6 +11,8 @@ public class SaveController : MonoBehaviour
     void Start()
     {
         saveLocation = Path.Combine(Application.persistentDataPath, "saveData.json");
+
+        LoadGame();
     }
 
     public void SaveGame()
@@ -21,5 +23,17 @@ public class SaveController : MonoBehaviour
         };
 
         File.WriteAllText(saveLocation, JsonUtility.ToJson(saveData));
+    }
+
+    public void LoadGame{
+        if(File.Exists(saveLocation)){
+            SaveData saveData = JsonUtility.FromJson<SaveData>(File.ReadAllText(saveLocation));
+
+            GameObject.FindGameObjectWithTag("Player").transform.position = saveData.playerPosition;
+
+            FindGameObjectOfType<CinemachineConfiner>().m_BoundingShape2D = GameObject.Find(saveData.mapBoundary).GetComponent<PolygonCollider2D>();
+        } else{
+            SaveGame();
+        }
     }
 }
