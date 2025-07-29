749b168373f62548a21a0baaf2bce144a7ee5763
Zainab-Ayoub <zainabzafar365@gmail.com>
2025-07-29 14:46:20 +0500

    start function

--- Assets/InventoryController.cs
+++ Assets/InventoryController.cs
@@ -2,9 +2,20 @@ using UnityEngine;
 
 public class InventoryController : MonoBehaviour
 {
-    
+    public GameObject inventoryPanel;
+    public GameObject slotPrefab;
+    public int slotCount;
+    public GameObject[] itemPrefabs;
+
     void Start()
     {
-        
+        for(int i=0; i < slotCount; i++){
+            Slot slot = Instantiate(slotPrefab[i], slot.transform);
+            if(i < itemPrefabs.Length){
+                GameObject item = Instantiate(itemPrefabs[i], slot.transform);
+                item.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                slot.currentItem = item;
+            }
+        }
     }
 }
