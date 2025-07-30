71fba01453a0d28679f98eac8a92a66b8927f6ee
Zainab-Ayoub <zainabzafar365@gmail.com>
2025-07-31 03:34:48 +0500

    get item + increment id

--- Assets/ItemDictionary.cs
+++ Assets/ItemDictionary.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class ItemDictionary : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public List<Item> itemPrefabs;
+    private Dictionary<int, GameObject> itemDictionary;
+ 
+    public void Awake()
     {
+        itemDictionary = new Dictionary<int, GameObject>();
+
+        // auto increment ids
+        for(int i=0; i < itemPrefabs; i++)
+        {
+            if(itemPrefabs[i] != null)
+            {
+                itemPrefabs[i].ID = i+1;
+            }
+        }
         
+        foreach(Item item in itemPrefabs)
+        {
+            itemDictionary[item.ID] = item.gameObject;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject GetItemPrefab(int itemID)
     {
-        
+        itemDictionary.TryGetValue(itemID, out GameObject prefab)
+        if(prefab == null)
+        {
+            Debug.LogWarning($"Item with ID {itemID} not found in dictionary.");
+        }
+        return prefab;
     }
 }
