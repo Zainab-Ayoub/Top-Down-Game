2e88f1272cec82a5cdb50f44eb8affb25b87ec2f
Zainab-Ayoub <zainabzafar365@gmail.com>
2025-08-04 12:14:52 +0500

    System.Linq

--- Assets/MapController_Manual.cs
+++ Assets/MapController_Manual.cs
@@ -1,8 +1,31 @@
 using UnityEngine;
 using UnityEngine.UI;
-using SYstem.Collections.Generic;
+using System.Collections.Generic;
+using System.Linq;
 
 public class MapController_Manual : MonoBehaviour
 {
     public static MapController_Manual Instance{ get; set; }
+
+    public GameObject mapParent;
+    List<Image> mapImages;
+
+    public Color highlightColour = Color.yellow;
+    public Color dimedColour = new Color(1f, 1f, 1f, 0.5f); 
+
+    public RectTransform playerIconTransform;
+
+    public void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        mapImages = mapParent.GetComponentInChildren<Image>().ToList();
+    }
 }
